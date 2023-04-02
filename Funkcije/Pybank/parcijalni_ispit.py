54cb3117cfd80e44583b3bd3403f7422a6b7d5b8
mayich92 <maja.matijasec555@gmail.com>
2023-04-02 17:25:12 +0200

    Parcijalni ispit

--- Funkcije/Pybank/parcijalni_ispit.py
+++ Funkcije/Pybank/parcijalni_ispit.py
@@ -17,7 +17,7 @@ def kreiranje_racuna():
         oib = input("OIB:")
     odgovorna_osoba = input("Unesite ime i prezime odgovorne osobe:")
     iznos = float(input("Unesite iznos po želji:"))
-    valuta = input("Valuta: (HRK/EUR):")
+    valuta = input("Valuta: (HRK/EUR):").upper()
     while valuta not in ["HRK","EUR"]:
         print("Pogrešna valuta, pokušajte ponovno!")
         valuta = input("Valuta: (HRK/EUR):")
@@ -32,7 +32,57 @@ def kreiranje_racuna():
         "oib": oib,
         "odgovorna_osoba": odgovorna_osoba,
         "stanje": iznos,
-        "valuta": valuta
+        "valuta": valuta,
+        "promet": []
     }
-    print("Račun uspješno kreiran.\nBroj računa: ", broj_racuna)
+    print(f"Račun uspješno kreiran.\nBroj računa: ", broj_racuna)
+kreiranje_racuna()
+
+def prikaz_stanja_racuna():
+    print("Prikaz stanja računa")
+    broj_racuna = input("Unesite broj računa: ")
+    if broj_racuna in racuni:
+        print("Trenutno stanje na računu: ", racuni[broj_racuna]["stanje"], racuni[broj_racuna]["valuta"])
+    else:
+        print("Račun ne postoji")
+prikaz_stanja_racuna()
+
+def prikaz_prometa_po_racunu():
+    print("Prikaz prometa po računu")
+    broj_racuna = input("Unesite broj računa: ")
+    if broj_racuna in racuni:
+        print("Promet po računu" , broj_racuna)
+        for transakcija in racuni[broj_racuna]["promet"]:
+            print(transakcija["datum"], "-", transakcija["opis"], "-", transakcija["iznos"],
+                  racuni[broj_racuna]["valuta"])
+        else:
+            print("Nema transakcija na ovom računu!")
+prikaz_prometa_po_racunu()
+
+def polog_novca_na_racun():
+    broj_racuna = input("Unesite broj računa: ")
+    if broj_racuna in racuni:
+        iznos = float(input("Unesite iznos za polog: "))
+        valuta = input("Valuta (EUR ili HRK): ")
+        print("Uspješno ste položili novce na Vaš račun!")
+
+polog_novca_na_racun()
+
+def podizanje_novaca_sa_racuna():
+    dnevni_limit_u_kunama = 5000
+    dnevni_limit_u_eurima = 655
+    broj_racuna = input("Unesite broj računa:")
+    iznos = float(input("Unesite iznos za isplatu: "))
+    valuta = input("Valuta (EUR ili HRK): ")
+    if broj_racuna in racuni and dnevni_limit_u_kunama <=5000 and dnevni_limit_u_eurima <=655 :
+        print("Uspješno ste podignuli novce sa računa.")
+    elif broj_racuna in racuni and dnevni_limit_u_kunama <=5000 and dnevni_limit_u_eurima <=655 :
+        print("Premašili ste Vaš dnevni limit.")
+podizanje_novaca_sa_racuna()
+
+
+
+
+
+
 
