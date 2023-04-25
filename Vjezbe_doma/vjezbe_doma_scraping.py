0cdb467baabc20691675857fc7cfe8f257562a55
mayich92 <maja.matijasec555@gmail.com>
2023-04-25 19:00:34 +0200

    zadaci

--- Vjezbe_doma/vjezbe_doma_scraping.py
+++ Vjezbe_doma/vjezbe_doma_scraping.py
@@ -6,28 +6,32 @@
 # Prodajni za devize: 7,229119
 # Aktualnu tečajnu listu scrapati s HPB poveznice  : https://www.hpb.hr/hr/tecajna-lista/2652
 
+
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://www.hpb.hr/hr/tecajna-lista/2652'
-response = requests.get(url)
+def get_exchange_rates(currency_code):
+    url = "https://www.hpb.hr/hr/tecajna-lista/2652"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
 
-soup = BeautifulSoup(response.text, 'html.parser')
-table = soup.find('table', attrs={'class': 'tecajnaLista'})
+    table = soup.find("table", {"class": "tecajnica-table"})
+    rows = table.tbody.find_all("tr")
 
-currency_code = input('Unesite kod valute (npr. USD, EUR, DKK): ').upper()
-rows = table.find_all('tr')
-for row in rows:
-    cells = row.find_all('td')
-    if cells[0].text == currency_code:
-        country = cells[1].text
-        buying_rate = cells[2].text
-        middle_rate = cells[3].text
-        selling_rate = cells[4].text
-        print(f'Trenutni tečaj za valutu {currency_code} (država: {country}):')
-        print(f'Kupovni za devize: {buying_rate}')
-        print(f'Srednji za devize: {middle_rate}')
-        print(f'Prodajni za devize: {selling_rate}')
-        break
-else:
-    print('Nepoznati kod valute.')
+    for row in rows:
+        cols = row.find_all("td")
+        if cols[0].text.strip() == currency_code:
+            country = cols[1].text.strip()
+            buying_rate = cols[2].text.strip().replace(",", ".")
+            middle_rate = cols[3].text.strip().replace(",", ".")
+            selling_rate = cols[4].text.strip().replace(",", ".")
+            print(f"Trenutni tečaj za valutu {currency_code} (država: {country}):")
+            print(f"Kupovni za devize: {buying_rate}")
+            print(f"Srednji za devize: {middle_rate}")
+            print(f"Prodajni za devize: {selling_rate}")
+            break
+    else:
+        print(f"Valuta {currency_code} nije pronađena u tečajnoj listi.")
+
+# Primjer poziva funkcije za dansk kronu
+get_exchange_rates("DKK")
